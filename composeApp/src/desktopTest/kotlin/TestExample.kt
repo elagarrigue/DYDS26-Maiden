84bbbecfec22410a61a161d78365b05b04b41f20
lucas <nahuellucasgarcia@gmail.com>
2026-05-13 14:59:46 -0300

    test: eliminar placeholder desktopTest

--- composeApp/src/desktopTest/kotlin/TestExample.kt
+++
@@ -1,7 +0,0 @@
-// Archivo de tests placeholder para el sourceSet desktopTest.
-// Reemplazar con tests reales siguiendo el Plan Etapa 2 (Stage 1)
-
-package edu.dyds.movies.test
-
-// TODO: agregar tests unitarios en este paquete. Actualmente se eliminó el contenido de ejemplo.
-
